a2aa02c89d3b57c3e0f1c68a5f64de094a50dd0f
klu0926 <lukuoyu@gmail.com>
2023-04-19 21:07:11 +0800

    project init

---
+++ app.js
@@ -0,0 +1,19 @@
+if (process.env.DOT_ENV !== 'production'){
+  require('dotenv').config()
+}
+
+const express = require('express')
+
+
+const app = express()
+const PORT = process.env.PORT
+
+
+app.get('/', (req, res)=> {
+  res.send('Hello world!')
+})
+
+
+app.listen(PORT, ()=>{
+  console.log(`server is live on http://localhost:${PORT}`)
+})
