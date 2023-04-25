ea49fdf8dc84bad218efc98dd7e5aac6733b674b
klu0926 <lukuoyu@gmail.com>
2023-04-25 22:59:56 +0800

    refacor: change seeders to use restaurantListIndex to generate restaruant data

--- models/seeds/seeders.js
+++ models/seeds/seeders.js
@@ -8,57 +8,59 @@ const bcrypt = require('bcryptjs')
 
 // 種子資料
 const RESTAURANTS_DATA = require('../data/restaurant.json').results
-const SEEDER_USER_1 = { email: 'user1@example.com', password: '12345678' }
-const SEEDER_USER_2 = { email: 'user2@example.com', password: '12345678' }
-const SEEDER_USERS = [SEEDER_USER_1, SEEDER_USER_2]
-
-// 使用幾個餐廳資料 (最多8個)
-const restaurantsAmount = 6
-
-// 種子使用者擁有的餐廳 (每個餐廳都需要分配到)
-const user1RestaurantList = [1, 2, 3]
-const user2RestaurantList = [4, 5, 6]
+const SEED_USER = [
+  {
+    name: 'user1',
+    email: 'user1@example.com',
+    password: '12345678',
+    restaurantList: [1, 2, 3]
+  },
+  {
+    name: 'user2',
+    email: 'user2@example.com',
+    password: '12345678',
+    restaurantList: [4, 5, 6]
+  },
+]
 
 db.once('open', async () => {
   // create users
   console.log('creating seeder users...')
-  const users = []
 
   return Promise.all(
-    SEEDER_USERS.map(seederUser => {
-      return bcrypt
-        .genSalt(10)
-        .then(salt => bcrypt.hash(seederUser.password, salt))
-        .then(hash => User.create({ email: seederUser.email, password: hash }))
-        .then(user => {
-          users.push(user)
+    SEED_USER.map(async (user) => {
+      try {
+        // 做出使用者
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(user.password, salt)
+        const createdUser = await User.create({
+          name: user.name,
+          email: user.email,
+          password: hash
         })
+        console.log(`user ${user.name} created!`)
+
+        // 使用 index 從json檔案 找出這名使用者全部餐廳
+        const userRestaurants = user.restaurantList.map(index => {
+          const restaurant = RESTAURANTS_DATA[index]
+          restaurant.userId = createdUser._id
+          return restaurant
+        })
+
+        // 做這名使用者全部的餐廳資料
+        await Restaurant.create(userRestaurants)
+        console.log(`${user.name}'s ${user.restaurantList.length} restaurants created!`)
+
+      } catch (err) {
+        console.log(err)
+      }
     })
   )
-    // create restaurant
     .then(() => {
-      console.log('creating seeder restaurants...')
-      // slice restaurant data
-      const sliceData = RESTAURANTS_DATA.slice(0, restaurantsAmount)
-
-      return Promise.all(
-        sliceData.map(data => {
-          // 餐廳登記為 使用者1
-          if (user1RestaurantList.includes(data.id)) {
-            data.userId = users[0]._id
-          }
-
-          // 餐廳登記為 使用者2
-          if (user2RestaurantList.includes(data.id)) {
-            data.userId = users[1]._id
-          }
-          // 做餐廳資料
-          return Restaurant.create(data)
-        })
-      )
-    })
-    .then(() => {
-      console.log('Seeder all done!')
+      console.log('all seeder data created!')
       process.exit()
+    })
+    .catch(err => {
+      console.log(err)
     })
 })
